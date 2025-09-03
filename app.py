f6d77f51d7e2bec88b3e1fb56a0e7941bb9cbd31
임지훈 <ljh081210@gmail.com>
2025-09-03 13:33:19 +0900

    Update app.py

--- app.py
+++ app.py
@@ -6,11 +6,12 @@ app = FastAPI()
 
 @app.get("/api", response_class=PlainTextResponse)
 def get_api():
-    return "product marking!"
+    return "product marking!!"
 
 @app.get("/health", response_class=PlainTextResponse)
 def health_check():
     return "ok"
 
 if __name__ == "__main__":
+
     uvicorn.run(app, host="0.0.0.0", port=8080, reload=True)
