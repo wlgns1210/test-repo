99e1929aa3bc79efbcf0bebe2286a2a91665def3
임지훈 <ljh081210@gmail.com>
2025-09-05 12:13:57 +0900

    Update app.py

--- app.py
+++ app.py
@@ -15,20 +15,5 @@ def health_check():
 if __name__ == "__main__":
 
     uvicorn.run(app, host="0.0.0.0", port=8080, reload=True)
+
 # updated at 1756792221
-# updated at 1756793288
-# updated at 1756795261
-# updated at 1756819537
-# updated at 1756857373
-# updated at 1756857575
-# updated at 1756857792
-# updated at 1756858077
-# updated at 1756859343
-# updated at 1756861880
-# updated at 1756862524
-# updated at 1756865939
-# updated at 1756866525
-# updated at 1756867502
-# updated at 1756867586
-# updated at 1756869289
-# updated at 1757041964
